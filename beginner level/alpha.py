fec917d413a36a7eb1bf0ebf0ecbee287b0a575e
RoyalRamesh <36730639+RoyalRamesh@users.noreply.github.com>
2018-02-23 14:17:28 +0530

    Create alpha.py

---
+++ beginner level/alpha.py
@@ -0,0 +1,7 @@
+k=input("enter an alphabet: ")
+if (k>='a' and k<='z'):
+	print("%s is a lowercase alphabet." % k)
+elif (k>='A' and k<='Z'):
+	print("%s is a uppercase alphabet." % k)
+else:
+  print("It is not an alphabet")
