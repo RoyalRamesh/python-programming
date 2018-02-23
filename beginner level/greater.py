584cccb5f14d382f77f62abf88e0ad199f7a2056
RoyalRamesh <36730639+RoyalRamesh@users.noreply.github.com>
2018-02-23 14:27:57 +0530

    Create greater.py

---
+++ beginner level/greater.py
@@ -0,0 +1,9 @@
+value1=int(input("Enter the value"))
+value2=int(input("enter the value"))
+value3=int(input("enter the value"))
+if (value1>value2) and (value1>value3):
+  print("%d is the greater number" %value1)
+elif(value2>value1) and (value2>value3):
+  print("%d is the greater number" %value2)
+else:
+  print("%d is the greater number" %value3)
